58dd9fa4fd3ae3c4b9256c72d97f85a8f3cb4392
Joshwa Fugett <jfugett27@gmail.com>
2014-11-19 11:58:17 -0800

    chore(work): Committing to work machine
    [skip ci]

--- test/benchmarks/index.js
+++ test/benchmarks/index.js
@@ -6,7 +6,15 @@ var gen = require('gen');
 suite('main', function * main () {
   bench('bench', function * innerBench() {
     yield gen.delay(5000);
-    console.log('bench');
+  }, {
+    concurrency: 1,
+    delay: 0,
+    iterations: 20,
+    mintime: 1000,
+    timeout: 2000
+  });
+  bench('bench2', function * innerBench() {
+    yield gen.delay(2000);
   }, {
     concurrency: 1,
     delay: 0,
