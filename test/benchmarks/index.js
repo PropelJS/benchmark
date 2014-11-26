a8470b83ab1e4357a936336c75d93040a6f564b7
Joshwa Fugett <jfugett27@gmail.com>
2014-11-25 17:30:41 -0800

    chore(home): Committing to continue at home
    [skip ci]

--- test/benchmarks/index.js
+++ test/benchmarks/index.js
@@ -5,31 +5,35 @@ var gen = require('gen');
 /* globals suite:false, bench:false */
 suite('main', function * main () {
   suite('nested', function * nested () {
-    bench('delay', function * delayed () {
+    bench('delay', function * delayed (timer) {
       yield gen.delay(100);
+      timer.mark('ok');
+      timer.mark('two');
+      timer.mark('3');
+      timer.mark('four');
+      timer.mark('last');
     });
   }, {
     minOps: 1000
   });
   bench('bench', function * innerBench (timer) {
     timer.mark('ok');
-    return true;
+    timer.mark('two');
+    timer.mark('3');
+    timer.mark('four');
+    timer.mark('last');
   });
 }, {
   comp: 'bench',
-  minOps: 1,
-//  iterations: 1000000,
-//  concurrency: 100000
+  minOps: 1
 });
 suite('error', function * errorSuite () {
-  bench('error', function * errorBench() {
-    return 'ok';
+  bench('error', function * errorBench(timer) {
+    return true;
   }, {
     minOps: 1e9
   });
 }, {
-  comp: 'any',
-//  iterations: 1000000,
-//  concurrency: 100000
+  comp: 'any'
 });
 
